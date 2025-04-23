c72f14ff1e469356f70964468cb1e5d203fbcf88
kkaris <karis.klas@gmail.com>
2025-04-23 15:49:02 -0700

    Add test

--- tests/test_api.py
+++ tests/test_api.py
@@ -5,15 +5,20 @@
 import unittest
 
 import depmap_downloader as dd
-from depmap_downloader import get_achilles_gene_dependencies_url
 
 
 class TestAPI(unittest.TestCase):
     """Test the API."""
 
+    def test_get_rnai_demeter(self):
+        """Test getting the latest RNAi Demeter file."""
+        url, version = dd.get_latest_rnai_url()
+        self.assertEqual("https://ndownloader.figshare.com/files/13515395", url)
+        self.assertEqual("demeter2_data_v6", version)
+
     def test_get_achilles(self):
         """Test getting the achilles URL."""
-        url = get_achilles_gene_dependencies_url(version="DepMap Public 21Q4")
+        url = dd.get_achilles_gene_dependencies_url(version="DepMap Public 21Q4")
         self.assertEqual("https://ndownloader.figshare.com/files/31315828", url)
 
     def test_essentiality(self):
